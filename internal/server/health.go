c2a3bba2c7c134943c6a5edc577af748d8677fc5
alex-chou <chou.alexander@gmail.com>
2018-10-04 01:26:44 -0700

    Add in method handler and clean up wrapped handlers.

--- internal/server/health.go
+++ internal/server/health.go
@@ -7,5 +7,5 @@ import (
 
 // Health responds with a default message.
 func (s *Server) Health(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello! The chatbot is healthy.")
+	fmt.Fprintln(w, "Hello! The chatbot is healthy.")
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -22,12 +22,27 @@ func NewServer() *Server {
 	server := &Server{
 		ServeMux: http.NewServeMux(),
 	}
-	server.HandleFunc("/health", server.Health)
+	server.HandleFunc(http.MethodGet, "/health", server.Health)
 	return server
 }
 
 // HandleFunc wraps the handler and passes it to the embedded ServeMux.
-func (s *Server) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
-	wrappedHandler := http.TimeoutHandler(http.HandlerFunc(handler), timeoutDuration, timeoutMessage)
+func (s *Server) HandleFunc(method, pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	wrappedHandler := http.Handler(http.HandlerFunc(handler))
+	wrappedHandler = MethodHandler(method, wrappedHandler)
+	wrappedHandler = http.TimeoutHandler(wrappedHandler, timeoutDuration, timeoutMessage)
 	s.ServeMux.Handle(pattern, wrappedHandler)
+}
+
+// MethodHandler invokes the input handler iff the request method matches the
+// input method.
+func MethodHandler(method string, handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == method {
+			handler.ServeHTTP(w, r)
+			return
+		}
+		http.NotFound(w, r)
+	})
+
 }
